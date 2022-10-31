5819e9dcfe216dd8314bbff0a5b074d9de41ba0b
Balalarga <lightman03@mail.ru>
2022-10-31 18:54:40 +0300

    Move directories defines to config

---
+++ Sources/Utils/SystemSettings.cpp
@@ -0,0 +1,19 @@
+﻿#include "SystemSettings.h"
+
+#include "Archives/Json/JsonArchive.h"
+
+namespace Ranok
+{
+SystemSettings::SystemSettings():
+	IConfig("SystemSettings", true)
+{
+}
+
+void SystemSettings::Serialize(JsonArchive& archive)
+{
+	archive.Serialize("resourcesDir", resourcesDir);
+	archive.Serialize("configsDir", configsDir);
+	archive.Serialize("defaultConfigsDir", defaultConfigsDir);
+	archive.Serialize("defaultLibrariesDir", defaultLibrariesDir);
+}
+}
---
+++ Sources/Utils/SystemSettings.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Config/IConfig.h"
+
+namespace Ranok
+{
+class SystemSettings: public IConfig
+{
+public:
+	SystemSettings();
+	void Serialize(JsonArchive& archive) override;
+
+	const std::string resourcesDir;
+	const std::string configsDir;
+	const std::string defaultConfigsDir;
+	const std::string defaultLibrariesDir;
+};
+}
